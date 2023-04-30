bfda2dd50ecf83dfc7a643aef088abfef0cd7e2c
xxtnguyn <113815061+xxtnguyn@users.noreply.github.com>
2023-04-30 15:18:24 +0700

    Create avatar.h

---
+++ avatar.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "header.h"
+#include "data.h"
+
+class avatar
+{
+public:
+	static void Cat(int, int);						// Vẽ mèo
+	static void amongUs(int, int, int, int);					// Vẽ nhân vật among us
+	static void specialAmongUs(int, int, int, int);		// Vẽ nhân vật among us siêu nhân
+};
