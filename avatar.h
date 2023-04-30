fd15e62b7e34dbfef99dd8855c26ad1cda049573
xxtnguyn <113815061+xxtnguyn@users.noreply.github.com>
2023-04-30 21:57:32 +0700

    Update avatar.h

--- avatar.h
+++ avatar.h
@@ -1,11 +1,14 @@
 #pragma once
 #include "header.h"
 #include "data.h"
+#include "common.h"
+#include "menu.h"
 
 class avatar
 {
 public:
 	static void Cat(int, int);						// Vẽ mèo
-	static void amongUs(int, int, int, int);					// Vẽ nhân vật among us
-	static void specialAmongUs(int, int, int, int);		// Vẽ nhân vật among us siêu nhân
+	static void rightAmongUs(int, int, int, int);					// Vẽ nhân vật among us phải
+	static void leftAmongUs(int, int, int, int);	// Vẽ nhân vật among us trái
+	static void leftAmongUsHead(int, int, int, int);	// Vẽ đầu among us
 };
