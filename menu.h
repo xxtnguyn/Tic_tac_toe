7e7d6fed624ef36adeb783bcda3a97370cb1753b
xxtnguyn <113815061+xxtnguyn@users.noreply.github.com>
2023-05-09 23:58:07 +0700

    Update menu.h

--- menu.h
+++ menu.h
@@ -20,6 +20,7 @@ public:
 	static void mainScreen();								//Màn hình chính
 	static void mainMenu();									//Menu
 	static void newGameMenu();									//Menu New Game
+	static void chooseAvatar();								// Chọn skin nhân vật
 	static void loadScreen();								//Màn hình load file
 	static void helpScreen();								//Màn hình hướng dẫn
 	static void aboutUS();									//Màn hình giới thiệu
@@ -30,5 +31,5 @@ public:
 	static void backMenu();									//Trở về Menu chính
 	static void changeOption(bool, bool);					//Thay đổi lựa chọn
 	//static void onlyOption();								//Lựa chọn duy nhất
-	//static void changeFile(int, vector<string>&, int&);		//Thay đổi file
+	static void changeFile(int, vector<string>&, int&);		//Thay đổi file
 };
