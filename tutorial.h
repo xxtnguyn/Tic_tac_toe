31e7c4ebd4155cb731b792b931527ff1cf269e2e
xxtnguyn <113815061+xxtnguyn@users.noreply.github.com>
2023-05-12 02:07:23 +0700

    Create tutorial.h

---
+++ tutorial.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "avatar.h"
+#include "header.h"
+#include "common.h"
+
+class tutorial
+{
+public:
+	static void tutorialIntro();								// Vẽ intro tutorial
+	static void tutorialStepOne();
+	static void tutorialStepTwo();
+	static void tutorialStepThree();
+	static void tutorialStepFour();
+	static void tutorialStepFive();
+};
