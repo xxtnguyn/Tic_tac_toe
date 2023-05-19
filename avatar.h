24a3afbaa7b53e696cc0772d633feca6b7f90c93
xxtnguyn <113815061+xxtnguyn@users.noreply.github.com>
2023-05-19 19:46:15 +0700

    Update avatar.h

--- avatar.h
+++ avatar.h
@@ -10,6 +10,6 @@ public:
 	static void rightAmongUs(int, int, int, int, int);	// Vẽ nhân vật among us phải
 	static void leftAmongUs(int, int, int, int, int, int);	// Vẽ nhân vật among us trái
 	static void leftAmongUsHead(int, int, int);		// Vẽ đầu among us
-	static void Bot(int, int);				// Vẽ con bot fithcmus
-	static void BotHead(int, int, int, int);		// Vẽ phần đầu con bot
+	static void Bot(int, int, int, int, int);						// Vẽ con bot fithcmus
+	static void BotHead(int, int, int, int);					// Vẽ phần đầu con bot
 };
