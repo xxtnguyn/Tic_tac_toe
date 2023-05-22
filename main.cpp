c2a7a6cfdf4c06402d1c33b13d6b28178055f068
xxtnguyn <113815061+xxtnguyn@users.noreply.github.com>
2023-05-22 11:06:22 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,5 +5,12 @@
 int main()
 {
 	common::setUpConsole();
+	_getch();
+	system("color F0");
+	menu::Tutorial();
+	Sleep(1000);
+	common::clearConsole();
+	draw::printIntro();
+	draw::printAnimatedBigXandBigO(96, 10, 1, 10);
 	menu::mainScreen();
 }
