220768fdf44e5e0ce5dd36241009f9a11a099b06
xxtnguyn <113815061+xxtnguyn@users.noreply.github.com>
2023-05-22 11:06:59 +0700

    Update menu.h

--- menu.h
+++ menu.h
@@ -3,6 +3,7 @@
 #include "common.h"
 #include "gameplay.h"
 #include "draw.h"
+#include "time.h"
 
 
 #include <unordered_map>
@@ -19,8 +20,7 @@ private:
 public:
 	static void mainScreen();								//Màn hình chính
 	static void mainMenu();									//Menu
-	static void newGameMenu();									//Menu New Game
-	static void chooseAvatar();								// Chọn skin nhân vật
+	static void newGameMenu();								//Menu New Game
 	static void loadScreen();								//Màn hình load file
 	static void helpScreen();								//Màn hình hướng dẫn
 	static void aboutUS();									//Màn hình giới thiệu
@@ -32,5 +32,6 @@ public:
 	static void changeOption(bool, bool);					//Thay đổi lựa chọn
 	//static void onlyOption();								//Lựa chọn duy nhất
 	static void changeFile(int, vector<string>&, int&);		//Thay đổi file
+	//Hàm sử dụng trong màn hình chọn avatar:
 	static void Tutorial();									//Hướng dẫn chơi game
 };
